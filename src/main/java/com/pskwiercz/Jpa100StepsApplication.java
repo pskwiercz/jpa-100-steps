782ab5049e22459ef0697fa332352d13b590c015
pskwiercz <przemek.skwiercz@gmail.com>
2018-08-29 14:39:16 +0200

    JPA in 100 steps - Initial Commit

---
+++ src/main/java/com/pskwiercz/Jpa100StepsApplication.java
@@ -0,0 +1,14 @@
+package com.pskwiercz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa100StepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpa100StepsApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/pskwiercz/Jpa100StepsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pskwiercz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Jpa100StepsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
